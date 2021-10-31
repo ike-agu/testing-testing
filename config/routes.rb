caa17230d7b7b4bb15fca7a0dd203fa400acd071
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-10-31 22:30:41 +0000

    route file modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+  root to: 'article_365s#index'
   resources :article_365s, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
